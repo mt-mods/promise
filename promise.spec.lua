cdaa9a729c15f2a6274c0912c1b9ceb53ef8c11e
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-04-11 11:17:08 +0200

    more tests

--- promise.spec.lua
+++ promise.spec.lua
@@ -1,9 +1,36 @@
 
-mtt.register("smoketest", function(callback)
+mtt.register("simple promise", function(callback)
     Promise.new(function(resolve)
         resolve(5)
     end):next(function(result)
         assert(result == 5)
+        callback()
+    end)
+end)
+
+mtt.register("returned promise", function(callback)
+    local p1 = Promise.new(function(resolve)
+        resolve(5)
+    end)
+
+    local p2 = Promise.new(function(resolve)
+        resolve(10)
+    end)
+
+    p1:next(function(result)
+        assert(result == 5)
+        return p2
+    end):next(function(result)
+        assert(result == 10)
+        callback()
+    end)
+end)
+
+mtt.register("error handling", function(callback)
+    Promise.new(function(_, reject)
+        reject("nope")
+    end):catch(function(err)
+        assert(err == "nope")
         callback()
     end)
 end)
