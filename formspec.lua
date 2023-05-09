b5bf649a6d8202ed15352588603100b21c217d69
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-05-09 13:05:47 +0200

    player scoped formspec callbacks and cleanups

--- formspec.lua
+++ formspec.lua
@@ -1,7 +1,14 @@
+-- playername -> { id => fn() }
 local formspec_promises = {}
 
-minetest.register_on_player_receive_fields(function(player, formname, fields)
-    local p = formspec_promises[formname]
+minetest.register_on_leaveplayer(function(player)
+    local playername = player:get_player_name()
+    formspec_promises[playername] = nil
+end)
+
+minetest.register_on_player_receive_fields(function(player, id, fields)
+    local playername = player:get_player_name()
+    local p = formspec_promises[id]
     if not p then
         return false
     end
@@ -12,14 +19,25 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
     })
 
     -- cleanup
-    formspec_promises[formname] = nil
+    local cb_map = formspec_promises[playername]
+    if cb_map then
+        cb_map[id] = nil
+    end
     return true
 end)
 
 function Promise.formspec(player, formspec)
     local p = Promise.new()
     local id = "" .. math.floor(math.random() * 100000)
-    formspec_promises[id] = p
+
+    local playername = player:get_player_name()
+    local cb_map = formspec_promises[playername]
+    if not cb_map then
+        -- create callback map
+        cb_map = {}
+        formspec_promises[playername] = cb_map
+    end
+    cb_map[id] = p
     minetest.show_formspec(player:get_player_name(), id, formspec)
     return p
 end
